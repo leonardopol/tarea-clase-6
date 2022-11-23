1b39443cf75b81d44839a43a4e6e7b1b6f68025d
leonardopol <leonardo_pol@hotmail.com>
2022-11-23 12:29:53 -0300

    factoriza botono enviar
    se crea htmlfor y cambia nombre a boton enviar

--- tarea1-clase-6.js
+++ tarea1-clase-6.js
@@ -5,3 +5,27 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
+
+document.querySelector("#boton-enviar").onclick = function(){
+    const $cantidadFamiliares = Number(document.querySelector("#cantidadFamiliares").value);
+    const $nodoDiv = document.querySelector("#mostrar-familiares");
+    const $labels = [];
+    const $inputs = [];
+    const $saltoDeLinea = [];
+    for(let i = 0; i < $cantidadFamiliares; i++){
+        $labels[i] = document.createElement("label");
+        $labels[i].id = `familiar${i}`;
+        $labels[i].htmlFor = $labels[i].id;
+        $textLabels = document.createTextNode(`Edad familiar ${i + 1}`);
+        $inputs[i] = document.createElement("input");
+        $inputs[i].type = "number";
+        $saltoDeLinea[i] = document.createElement("br");
+        $nodoDiv.appendChild($saltoDeLinea[i]);
+        $nodoDiv.appendChild($labels[i]);
+        $nodoLabel = document.querySelector(`#familiar${i}`);
+        $nodoLabel.appendChild($textLabels);
+        
+        $nodoDiv.appendChild($inputs[i]);
+    } 
+    return false;
+}
